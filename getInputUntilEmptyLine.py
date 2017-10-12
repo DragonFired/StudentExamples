344cb35068760eb8cdfb528f92d1fc54549d8977
Arana Fireheart <arana.fireheart@newbury.edu>
2017-10-12 12:39:04 -0400

    added an example for getting user input until an empty line is entered.

---
+++ getInputUntilEmptyLine.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+__author__ = 'Arana Fireheart'
+
+userInput = input("Please enter a number: ")
+while userInput != '':
+    print(int(userInput))
+    userInput = input("Please enter a number: ")
